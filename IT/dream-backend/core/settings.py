a1ec9d3de3029e110e013c7c62102dd3fd568f6b
Gianmario <10613872@polimi.it>
2022-01-18 22:22:14 +0100

    added Cors and configuration needed to handle request from localhost:3000

--- IT/dream-backend/core/settings.py
+++ IT/dream-backend/core/settings.py
@@ -40,9 +40,13 @@ INSTALLED_APPS = [
     'forum',           # added
     'forum_api',       # added
     'rest_framework',  # added
+    'corsheaders',      # added
 ]
 
+# cors setup here https://pypi.org/project/django-cors-headers/
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",        # added
+    "django.middleware.common.CommonMiddleware",    # added
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -132,3 +136,9 @@ REST_FRAMEWORK = {
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# cors setup, here we can specify the allowed origins
+CORS_ALLOWED_ORIGINS = [
+    "https://example.com",
+    "http://localhost:3000",
+]
