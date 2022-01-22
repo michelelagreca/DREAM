23c6dd8788ad96ec98e74aae6b74c89a49d0de4c
Gianmario <10613872@polimi.it>
2022-01-23 00:48:18 +0100

    updated auth code logic for farmer and policymaker
    now after a registration the used authcode is unvalidated

--- IT/dream-backend/users/serializers.py
+++ IT/dream-backend/users/serializers.py
@@ -6,6 +6,30 @@ from .models import CustomUser, AuthCodeAgronomist, AuthCodeFarmer, AuthCodePoli
 # they also specify the (JSON) shape of the data provided to the callers
 # DOC serializers: https://www.django-rest-framework.org/api-guide/serializers/
 
+# auxiliary function to match the auth code with a specific user
+def checkUserAuthCode(user_data, code_object):
+    if user_data['first_name'] != str(code_object.first_name):
+        raise serializers.ValidationError("Invalid authorization code")
+
+    if user_data['last_name'] != str(code_object.last_name):
+        raise serializers.ValidationError("Invalid authorization code")
+
+    if not bool(code_object.isValid):
+        raise serializers.ValidationError("Authorization code not valid")
+
+
+# make authcode not valid, given the role and the validated data of an user
+def disableAuthCode(data):
+    if data['role'] == 'farmer':
+        auth_code_object = AuthCodeFarmer.objects.get(pk=data['auth_code'])
+        auth_code_object.isValid = False
+        auth_code_object.save()
+
+    elif data['role'] == 'policymaker':
+        auth_code_object = AuthCodePolicyMaker.objects.get(pk=data['auth_code'])
+        auth_code_object.isValid = False
+        auth_code_object.save()
+
 
 class RegistrationSerializer(serializers.ModelSerializer):
     class Meta:
@@ -17,34 +41,66 @@ class RegistrationSerializer(serializers.ModelSerializer):
     def validate(self, data):
         """
         Function called to validate the data before create.
-        DOC: https://www.django-rest-framework.org/api-guide/serializers/#validation
         """
+        # set username, it is implemented for robustness, to allow the usage of user_name in the future
+        data['user_name'] = data['email']
+
         # validate role string
-        roles = ['farmer', 'policymaker', 'agronomist']
+        roles = ['farmer', 'agronomist', 'policymaker']
         if not data['role'] in roles:
             raise serializers.ValidationError("Invalid role")
 
-        # if data['role'] == roles[0]:
-        print(data)
+        # validate authorization code
+        if data['role'] == roles[0]:
+            try:
+                auth_code_object = AuthCodeFarmer.objects.get(pk=data['auth_code'])
+                checkUserAuthCode(user_data=data, code_object=auth_code_object) # assign auth code zone to farmer
+                data['zone'] = auth_code_object.zone
+                data['district'] = auth_code_object.zone.district
+            except AuthCodeFarmer.DoesNotExist:
+                raise serializers.ValidationError("Invalid farmer authorization code")
 
-        try:
-            auth_code = AuthCodeFarmer.objects.get(pk=data['auth_code'])
-            print(auth_code)
-        except AuthCodeFarmer.DoesNotExist:
-            raise serializers.ValidationError("Invalid authorization code")
+        elif data['role'] == roles[1]:
+            try:
+                auth_code_object = AuthCodeAgronomist.objects.get(pk=data['auth_code'])
+            except AuthCodeAgronomist.DoesNotExist:
+                raise serializers.ValidationError("Invalid agronomist authorization code")
+            raise serializers.ValidationError("Registration for agronomist will be ready soon...")
+
+        else:
+            try:
+                auth_code_object = AuthCodePolicyMaker.objects.get(pk=data['auth_code'])
+                checkUserAuthCode(user_data=data, code_object=auth_code_object)
+                data['district'] = auth_code_object.district    # assign auth code district to policymaker
+            except AuthCodePolicyMaker.DoesNotExist:
+                raise serializers.ValidationError("Invalid policy maker authorization code")
 
         return data
 
     def create(self, validated_data):
-        # validate user data
+        # dynamically create district/zone field based on the user type
+        geo_args = {}
+        if validated_data['role'] == 'farmer':
+            geo_args = {'zone': validated_data['zone'], 'district': validated_data['district']}
+        elif validated_data['role'] == 'policymaker':
+            geo_args = {'district': validated_data['district']}
+
+        # create user object
         user = CustomUser(
             first_name=validated_data['first_name'],
             email=validated_data['email'],
             last_name=validated_data['last_name'],
             auth_code=validated_data['auth_code'],
             role=validated_data['role'],
+            user_name=validated_data['user_name'],
+            **geo_args
         )
-        # password saved hashed
+
+        # save password hashed
         user.set_password(validated_data['password'])
         user.save()
+
+        # disable authcode
+        disableAuthCode(data=validated_data)
+        print("@@ Serializer execution...")
         return user
