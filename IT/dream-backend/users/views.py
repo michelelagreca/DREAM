8c8c216c43c9805e7e1bcbf4fd49414e28931877
Gianmario <10613872@polimi.it>
2022-01-24 01:04:26 +0100

    added customization of jwt response with role

--- IT/dream-backend/users/views.py
+++ IT/dream-backend/users/views.py
@@ -1,8 +1,46 @@
 from .serializers import RegistrationSerializer
 from rest_framework import generics
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 # Create your views here.
 class PostUser(generics.CreateAPIView):
     # queryset = Question.objects.all()
     serializer_class = RegistrationSerializer
+
+
+# customization of the token jwt response
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        refresh = self.get_token(self.user)
+
+        # standard response
+        data['refresh'] = str(refresh)
+        data['access'] = str(refresh.access_token)
+
+        # extra responses
+        data['role'] = self.user.role
+
+        return data
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+
+class BlacklistTokenUpdateView(APIView):
+
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh_token"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
