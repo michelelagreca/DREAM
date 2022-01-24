dedd20e481192386c53ed5e31aa0cd1ab75288a7
Gianmario <10613872@polimi.it>
2022-01-24 11:50:22 +0100

    fixed merge problem

--- IT/dream-backend/report/admin.py
+++ IT/dream-backend/report/admin.py
@@ -1,14 +1,11 @@
 from django.contrib import admin
-from . import models
+from .models import HarvestReport
 
 
 # This part deals with what can be seen from the admin administration portal
 # add possibility in admin console to create new models
-@admin.register(models.HarvestReport)
+@admin.register(HarvestReport)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('firstName', 'lastName', 'date', 'id', 'status', 'slug', 'author')
     #prepopulated_fields = {'slug': ('title',), }
 
-
-admin.site.register(models.CropCategory)
-admin.site.register(models.Zone)
--- IT/dream-backend/report/models.py
+++ IT/dream-backend/report/models.py
@@ -1,43 +1,30 @@
-#Harvest_report
-
+# Harvest_report
 from django.db import models
-from django.contrib.auth.models import User
 from django.utils import timezone
+from users.models import CustomUser, Zone
+from forum.models import Category
 
 # Create your models here.
 
-class CropCategory(models.Model):
-    nameCategory = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.nameCategory
-
-class Zone (models.Model):
-    nameZone = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.nameZone
 
 class HarvestReport(models.Model):
-
     class ReportObjects(models.Manager):
         def get_queryset(self):
-            return super().get_queryset() .filter(status='published')
+            return super().get_queryset().filter(status='published')
 
     options = (
         ('draft', 'Draft'),
         ('published', 'Published'),
     )
 
-    
     firstName = models.CharField(max_length=50)
     lastName = models.CharField(max_length=50)
     date = models.DateTimeField()
     zone = models.ForeignKey(
         Zone, on_delete=models.PROTECT, default=1)
-    
-    cropCategory = models.ForeignKey(
-        CropCategory, on_delete=models.PROTECT, default=1)
+
+    category = models.ForeignKey(
+        Category, on_delete=models.PROTECT, default=1)
     cropName = models.CharField(max_length=50)
     quantity = models.FloatField()
     genericProblems = models.CharField(max_length=500)
@@ -46,7 +33,7 @@ class HarvestReport(models.Model):
     slug = models.SlugField(max_length=250, unique_for_date='published')
     published = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='report')
+        CustomUser, on_delete=models.CASCADE, related_name='report')
     status = models.CharField(
         max_length=10, choices=options, default='published')
     objects = models.Manager()  # default manager
@@ -56,4 +43,4 @@ class HarvestReport(models.Model):
         ordering = ('-published',)
 
     def __str__(self):
-       return self
+        return self
