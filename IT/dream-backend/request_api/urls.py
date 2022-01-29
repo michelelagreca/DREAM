ffb055bd807d6de67a090ef09ffd0d1e74004bce
Gianmario <10613872@polimi.it>
2022-01-29 18:44:47 +0100

    updated hr with hr status update logic for farmers

--- IT/dream-backend/request_api/urls.py
+++ IT/dream-backend/request_api/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import RequestList, RequestDetail, send_hr_farmer
+from .views import RequestList, RequestDetail, send_hr_farmer, hr_list_farmer, change_status_hr_farmer
 
 app_name = 'request_api'  # changed
 
 # we get redirected here from the core
 # here we define the application (api in this case) endpoints
 urlpatterns = [
-    path('sending_hr_farmer/', send_hr_farmer, name='send_hr_farmer')
+    path('sending_hr_farmer/', send_hr_farmer, name='send_hr_farmer'),
+    path('all_hr/', hr_list_farmer, name='read_hr_farmer'),
+    path('changing_status_hr_farmer/', change_status_hr_farmer, name='change_status_hr_farmer'),
+
 ]
--- IT/dream-backend/request_api/views.py
+++ IT/dream-backend/request_api/views.py
@@ -4,9 +4,10 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import generics, status
 from rest_framework.decorators import api_view
-from request.models import HelpRequest
+from request.models import HelpRequest, HR_OPTIONS_EXT
 from users.models import CustomUser
-from .serializers import HRSerializer
+from .serializers import HRSerializer, HRChangeStatusSerializer
+from django.db.models import Q
 
 
 # import here all the needed DB models
@@ -54,22 +55,68 @@ def send_hr_farmer(request):
     if len(hr_receivers) <= 0:
         return Response(data="No farmer nearby", status=status.HTTP_204_NO_CONTENT)
 
-    #TODO manage timestamp
+    # TODO manage timestamp
     for receiver_user in hr_receivers:
         new_request = HelpRequest(
             title=incoming_hr.validated_data['title'],
             # timestamp=incoming_hr.validated_data['timestamp'],
             content=incoming_hr.validated_data['content'],
-            #slug=incoming_hr.validated_data['slug'],
-            #published=incoming_hr.validated_data['published'],
+            # slug=incoming_hr.validated_data['slug'],
+            # published=incoming_hr.validated_data['published'],
             author=request.user,
             receiver=receiver_user,
-            status=incoming_hr.validated_data['status']
+            status='not_accepted'
         )
         new_request.save()
 
     farmer_number = len(hr_receivers)
     return Response(data="HR successfully sent to " + str(farmer_number), status=status.HTTP_200_OK)
+
+
+@api_view(['POST'])
+def change_status_hr_farmer(request):
+    # pass http request data to custom serializer
+    hr_serializer = HRChangeStatusSerializer(data=request.data)
+
+    # check validation
+    if not hr_serializer.is_valid():
+        return Response("Invalid Request", status=status.HTTP_400_BAD_REQUEST)
+
+    # read validated data
+    hr_id = hr_serializer.validated_data["hr_id"]
+    hr_status = hr_serializer.validated_data["status"]
+
+    if hr_status not in HR_OPTIONS_EXT:
+        return Response("Invalid Option", status=status.HTTP_400_BAD_REQUEST)
+
+    try:
+        hr = HelpRequest.objects.get(pk=hr_id)
+    except HelpRequest.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.user != hr.author and request.user != hr.receiver:
+        return Response(data="You cannot modify this hr", status=status.HTTP_401_UNAUTHORIZED)
+
+    hr.status = hr_status
+    hr.save()
+
+    return Response(data="HR status updated", status=status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+def hr_list_farmer(request):
+    # filter hr by user
+    qs_dict = HelpRequest.objects.filter(
+        Q(author=request.user) | Q(receiver=request.user)).values()  # get queryset in dictionary form
+
+    # add extra field to determine if the user is the sender or receiver of the hr
+    for hr in qs_dict:
+        if hr['author_id'] == request.user.id:
+            hr['is_sender'] = True
+        else:
+            hr['is_sender'] = False
+
+    return Response(data=qs_dict, status=status.HTTP_200_OK, content_type='application/json')
 
 
 # ADD HERE GENERIC VIEW FOR DEBUGGING PORPOUSE, THEY ARE INCLUDED IN THE REST FRAMEWORK
